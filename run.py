e2d1d270381d9f6894a8f6af65a112837cee6b12
CaviedesGitHub <l.padillac2@uniandes.edu.co>
2022-06-08 09:48:41 -0500

    Lección3

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
 from turtle import title
 from flask import Flask, render_template, request, redirect, url_for
+from forms import SignupForm, PostForm, ExamenForm
 
 app=Flask(__name__)
+app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
 
 posts=[]
+examenes=[]
 
 @app.route("/")
 def index():
-    return render_template("index.html", num_posts=len(posts))
+    return render_template("index.html", posts=posts)
     #return "{} posts".format(len(posts))
 
 @app.route("/p/<string:slug>/")
@@ -15,21 +18,52 @@ def show_post(slug):
     return render_template("post_view.html", slug_title=slug)
     #return "Mostrando el post {}".format(slug)
 
-@app.route("/admin/post/")
-@app.route("/admin/post/<int:post_id>/")
-def post_form(post_id=None):
-    return render_template("admin/post_form.html", post_id=post_id)
-    #return "post_form {}".format(post_id)
+@app.route("/admin/post/", methods=['GET', 'POST'], defaults={'post_id': None})
+@app.route("/admin/post/<int:post_id>/", methods=['GET', 'POST'])
+def post_form(post_id):
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        title_slug = form.title_slug.data
+        content = form.content.data
+
+        post = {'title': title, 'title_slug': title_slug, 'content': content}
+        posts.append(post)
+
+        return redirect(url_for('index'))
+    return render_template("admin/post_form.html", form=form)    
     
 @app.route("/signup/", methods=["GET", "POST"])
 def show_signup_form():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        password = request.form['password']
+    form = SignupForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        password = form.password.data
 
         next = request.args.get('next', None)
         if next:
             return redirect(next)
         return redirect(url_for('index'))
-    return render_template("signup_form.html")
+    return render_template("signup_form.html", form=form)
+
+@app.route("/examen/", methods=["GET", "POST"], defaults={'exam_id':None})
+@app.route("/examen/<int:exam_id>/", methods=['GET', 'POST'])
+def exam_form(exam_id):
+    form=ExamenForm()
+    if form.validate_on_submit():
+        cups=form.cups.data
+        nombre=form.nombre.data
+        
+        examen = {'cups': cups, 'nombre': nombre}
+        examenes.append(examen)
+        
+        next=request.args.get('next', None)
+        if next:
+            return redirect(next)
+        return redirect(url_for('lista_examenes'))
+    return render_template("exam_form.html", form=form)
+
+@app.route("/ListaExamenes")
+def lista_examenes():
+    return render_template("examenes_list.html", examenes=examenes)
