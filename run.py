57690b9c78bc409660be95a8bcf37f5666adf67b
CaviedesGitHub <l.padillac2@uniandes.edu.co>
2022-06-07 20:34:10 -0500

    Primera parte leccion2

--- run.py
+++ run.py
@@ -2,6 +2,17 @@ from flask import Flask
 
 app=Flask(__name__)
 
+posts=[]
+
 @app.route("/")
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return "{} posts".format(len(posts))
+
+@app.route("/p/<string:slug>/")
+def show_post(slug):
+    return "Mostrando el Post {}".format(slug)
+
+@app.route("/admin/post/")
+@app.route("/admin/post/<int:post_id>/")
+def post_form(post_id=None):
+    return "post_form {}".format(post_id)
