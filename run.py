7e7503006aac1eba2b95e881b1be26d980771f5b
CaviedesGitHub <l.padillac2@uniandes.edu.co>
2022-06-07 21:59:49 -0500

    Primera parte leccion3

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from turtle import title
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app=Flask(__name__)
 
@@ -20,3 +20,16 @@ def show_post(slug):
 def post_form(post_id=None):
     return render_template("admin/post_form.html", post_id=post_id)
     #return "post_form {}".format(post_id)
+    
+@app.route("/signup/", methods=["GET", "POST"])
+def show_signup_form():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        next = request.args.get('next', None)
+        if next:
+            return redirect(next)
+        return redirect(url_for('index'))
+    return render_template("signup_form.html")
