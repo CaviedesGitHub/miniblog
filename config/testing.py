b5d81af7c43a57ed1730f6cbc1c3bcd06fea4925
CaviedesGitHub <l.padillac2@uniandes.edu.co>
2022-06-16 22:19:54 -0500

    Leccion12

--- config/testing.py
+++ config/testing.py
@@ -8,4 +8,6 @@ DEBUG = True
 
 APP_ENV = APP_ENV_TESTING
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://db_user:db_pass@host:port/db_name'
+WTF_CSRF_ENABLED = False
+
+#SQLALCHEMY_DATABASE_URI = 'postgresql://db_user:db_pass@host:port/db_name'
